41ffbf15065c39b46793e5be5ef670815967e333
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2023-11-15 10:01:36 -0300

    Refactored to use Multer

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const express = require('express');
 const app = express();
-const fileUpload = require('express-fileupload');
+const multer = require('multer');
 const PORT = process.env.PORT || 3500;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(fileUpload());
+
+//This will rename the file with a random and unique suffix, while it maintain the original extension
+const storage = multer.diskStorage({
+    destination:'./uploads/',
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const originalExtesion = file.originalname.split('.').pop();
+        cb(null, `${uniqueSuffix}.${originalExtesion}`);
+    }
+});
+
+const upload = multer({storage: storage});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/upload', (req, res) => {
-    //Check if there is a uploaded file
-    if (!req.files || !req.files.uploadedFile) {
-        console.log(req);
-        return res.status(400).json({error: "No file uploaded"});
+//Example with multer
+app.post('/upload', upload.single('uploadedFile'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({Error: 'No file uploaded'});
     }
+    console.log('File uploaded sucessfully: ', req.file);
+    
+    res.send('File uploaded sucessfully');
+})
 
-    const uploadedFile = req.files.uploadedFile;
-    const filePath = './uploads/' + uploadedFile.name;
 
-    uploadedFile.mv(filePath, (err) => {
-        if (err) {
-            console.error('Error uploading file: ', err);
-            return res.status(500).json({error: 'Error uploading file'});
-        }
-        console.log('File uploaded sucessfully');
-        res.status(200).json({message: 'File uploaded sucessfully'});
-    });
-});
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
